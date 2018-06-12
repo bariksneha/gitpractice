1395616891c82521f4b055d7079d247f0978f0be
sneha <snehalatabarik7@gmail.com>
2018-06-12 15:26:08 +0530

    connection

---
+++ JdbcApps/src/com/nt/jdbc/ConnectionJdbc.java
@@ -0,0 +1,31 @@
+package com.nt.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJdbc {
+	public static void main(String[] args) {
+		Connection con=null;
+		try {
+		//register the driver
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//establish the connection
+		con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
+		if(con!=null)
+			System.out.println("connection established");
+		else
+			System.out.println("connection is not established");
+		}
+		catch(SQLException se){
+			se.printStackTrace();
+		}
+		catch(ClassNotFoundException ce) {
+			ce.printStackTrace();
+			
+		}
+		
+		
+	}//main()
+
+}//class
